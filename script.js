301a8096d39b300d7dcb796e59efa90a3db2f5a6
Jen Ox <jnochsner26@gmail.com>
2022-05-30 20:48:53 -0600

    Create contact form for contact page.
    Still need to create way to send forms to email address when submitted.

--- script.js
+++ script.js
@@ -8,8 +8,52 @@ document.addEventListener("DOMContentLoaded", function() {
     fields.message = document.getElementById('message');
 })
 
+
+
 function isNotEmpty(value) {
     if (value == null || typeof value == 'undefined') return false;
 
     return (value.length > 0);
+}
+
+function fieldValidation(field, validationFunction) {
+    if (field == null) return false;
+
+    let isFieldValid = validationFunction(field.value);
+    if (!isFieldValid) {
+        field.className = 'placeholderRed';
+    } else {
+        field.className = '';
+    }
+    return isFieldValid;
+}
+
+function isValid() {
+    let valid = true;
+
+    valid &= fieldValidation(fields.firstName, isNotEmpty);
+    valid &= fieldValidation(fields.lastName, isNotEmpty);
+    valid &= fieldValidation(fields.phoneNum, isNotEmpty);
+    valid &= fieldValidation(fields.email, isNotEmpty);
+    valid &= fieldValidation(fields.message, isNotEmpty);
+
+    return valid;
+}
+
+class User {
+    constructor(firstName, lastName, phoneNum, email, message) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.message = message;
+    }
+}
+
+function sendContact() {
+    if (isValid()) {
+        alert('Thank you! Your form has been submitted!');
+    } else {
+        alert("Please complete all fields.")
+    }
 }
